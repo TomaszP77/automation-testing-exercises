132324ad6f959134041831bc37a0b4a4f314c409
Tomasz <feat24@wp.pl>
2023-11-28 11:31:52 +0100

    Modify DriverFactory

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -3,11 +3,31 @@ package utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 
+    private static WebDriver driver;
+
     public static WebDriver getDriver() {
-        WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();
+        driver = selectDriver("Chrome");
+        return driver;
+    }
+
+    public static WebDriver selectDriver(String browserName) {
+        WebDriver driver = null;
+
+        if (browserName.equalsIgnoreCase("Chrome")) {
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        } else if (browserName.equalsIgnoreCase("Firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        } else if (browserName.equalsIgnoreCase("Edge")) {
+            WebDriverManager.edgedriver().setup();
+            driver = new EdgeDriver();
+        }
+        return driver;
     }
 }
