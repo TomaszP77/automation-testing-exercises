98a1a49810f69ed8f9af82460a0de87089ed749a
Tomasz <feat24@wp.pl>
2024-02-10 05:46:22 +0100

    Add ClickingOnItems

---
+++ src/test/java/seleniumcodingexercises/tests/ClickingOnItems.java
@@ -0,0 +1,15 @@
+package seleniumcodingexercises.tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import seleniumcodingexercises.utils.BaseTest;
+
+public class ClickingOnItems extends BaseTest {
+
+    private WebDriver driver;
+
+    @Test
+    public void performAction() {
+        openSeleniumDemo();
+    }
+}
