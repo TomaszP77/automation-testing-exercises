16fae233254365942845d0a1174001549e6d0e91
Tomasz <feat24@wp.pl>
2024-02-16 08:06:54 +0100

    Modify exerciseOfClickingOnElements assertion

--- src/test/java/seleniumcodingexercises/tests/SendKeys.java
+++ src/test/java/seleniumcodingexercises/tests/SendKeys.java
@@ -3,7 +3,11 @@ package seleniumcodingexercises.tests;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import seleniumcodingexercises.utils.BaseTest;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SendKeys extends BaseTest {
 
@@ -17,7 +21,14 @@ public class SendKeys extends BaseTest {
         driver.findElement(By.id("reg_password")).sendKeys("password1234567!!!");
         driver.findElement(By.name("register")).click();
 
-        String assertionMessage = driver.findElement(By.cssSelector(".entry-title")).getText();
-        Assertions.assertEquals("My account", assertionMessage);
+        List<WebElement> assertion = driver.findElements(By.cssSelector(".woocommerce-MyAccount-navigation ul li a"));
+        assertion.stream().map(el -> el.getAttribute("textContent")).collect(Collectors.toList());
+
+        Assertions.assertEquals("Dashboard", assertion.get(0).getText());
+        Assertions.assertEquals("Orders", assertion.get(1).getText());
+        Assertions.assertEquals("Downloads", assertion.get(2).getText());
+        Assertions.assertEquals("Addresses", assertion.get(3).getText());
+        Assertions.assertEquals("Account details", assertion.get(4).getText());
+        Assertions.assertEquals("Logout", assertion.get(5).getText());
     }
 }
