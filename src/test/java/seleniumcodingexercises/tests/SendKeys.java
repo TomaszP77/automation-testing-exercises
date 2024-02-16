97ba2727e9e7fcd56edf963bf7b05824b86d4532
Tomasz <feat24@wp.pl>
2024-02-16 06:47:14 +0100

    Add exerciseOfClickingOnElements

--- src/test/java/seleniumcodingexercises/tests/SendKeys.java
+++ src/test/java/seleniumcodingexercises/tests/SendKeys.java
@@ -1,4 +1,19 @@
 package seleniumcodingexercises.tests;
 
-public class SendKeys {
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import seleniumcodingexercises.utils.BaseTest;
+
+public class SendKeys extends BaseTest {
+
+    @Test
+    public void exerciseOfClickingOnElements() {
+        openSeleniumDemo();
+
+        int randomEmail = (int) (Math.random() * 1000);
+        driver.findElement(By.id("menu-item-22")).click();
+        driver.findElement(By.id("reg_email")).sendKeys("abcd" + randomEmail + "@efg.hij");
+        driver.findElement(By.id("reg_password")).sendKeys("password1234567!!!");
+        driver.findElement(By.name("register")).click();
+    }
 }
