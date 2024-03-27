bc7facfa0a9b7bf4fb5d2bb18a9365722ce366d7
Tomasz <feat24@wp.pl>
2024-03-27 20:52:48 +0100

    Add: ShoppingCartTests addProductTooCart

--- src/test/java/selenide/prestashop/pages/HomePage.java
+++ src/test/java/selenide/prestashop/pages/HomePage.java
@@ -1,10 +1,21 @@
 package selenide.prestashop.pages;
 
-import static com.codeborne.selenide.Selenide.$x;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.*;
 
 public class HomePage {
 
     public void openRegistrationPage() {
         $x("//span[text()='Sign in']").click();
+    }
+
+    public void moveTheCursorToClothes() {
+        SelenideElement element = $("[id=\"category-3\"]");
+        actions().moveToElement(element).perform();
+    }
+
+    public void clickOnCategoryMen() {
+        $("[id=\"category-4\"]").click();
     }
 }
